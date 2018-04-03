31f30c618f808d5c0ae6214063fd8f476fc91264
Jeff Ching <chingor@google.com>
2018-04-03 12:36:15 -0700

    Convert Links, TimeEvents, Status

--- src/Stackdriver/SpanConverter.php
+++ src/Stackdriver/SpanConverter.php
@@ -17,8 +17,16 @@
 
 namespace OpenCensus\Trace\Exporter\Stackdriver;
 
+use Google\Cloud\Trace\Annotation;
+use Google\Cloud\Trace\Link;
+use Google\Cloud\Trace\MessageEvent;
 use Google\Cloud\Trace\Span;
+use Google\Cloud\Trace\Status;
+use OpenCensus\Trace\Annotation as OCAnnotation;
+use OpenCensus\Trace\Link as OCLink;
+use OpenCensus\Trace\MessageEvent as OCMessageEvent;
 use OpenCensus\Trace\Span as OCSpan;
+use OpenCensus\Trace\Status as OCStatus;
 use OpenCensus\Trace\SpanData;
 
 /**
@@ -35,6 +43,16 @@ class SpanConverter
         OCSpan::ATTRIBUTE_USER_AGENT => '/http/user_agent',
         OCSpan::ATTRIBUTE_STATUS_CODE => '/http/status_code'
     ];
+    const LINK_TYPE_MAP = [
+        OCLink::TYPE_UNSPECIFIED => Link::TYPE_UNSPECIFIED,
+        OCLink::TYPE_CHILD_LINKED_SPAN => Link::TYPE_CHILD_LINKED_SPAN,
+        OCLink::TYPE_PARENT_LINKED_SPAN => Link::TYPE_PARENT_LINKED_SPAN
+    ];
+    const MESSAGE_TYPE_MAP = [
+        OCMessageEvent::TYPE_UNSPECIFIED => MessageEvent::TYPE_UNSPECIFIED,
+        OCMessageEvent::TYPE_SENT => MessageEvent::TYPE_SENT,
+        OCMessageEvent::TYPE_RECEIVED => MessageEvent::TYPE_RECEIVED
+    ];
 
     /**
      * Convert an OpenCensus SpanData to its Stackdriver Trace representation.
@@ -46,15 +64,21 @@ class SpanConverter
      */
     public static function convertSpan(SpanData $span)
     {
-        return new Span($span->traceId(), [
+        $spanOptions = [
             'name' => $span->name(),
             'startTime' => $span->startTime(),
             'endTime' => $span->endTime(),
             'spanId' => $span->spanId(),
-            'parentSpanId' => $span->parentSpanId(),
             'attributes' => self::convertAttributes($span->attributes()),
-            'stackTrace' => $span->stackTrace()
-        ]);
+            'stackTrace' => $span->stackTrace(),
+            'links' => self::convertLinks($span->links()),
+            'timeEvents' => self::convertTimeEvents($span->timeEvents()),
+            'status' => self::convertStatus($span->status())
+        ];
+        if ($span->parentSpanId()) {
+            $spanOptions['parentSpanId'] = $span->parentSpanId();
+        }
+        return new Span($span->traceId(), $spanOptions);
     }
 
     private static function convertAttributes(array $attributes)
@@ -68,5 +92,53 @@ class SpanConverter
             }
         }
         return $newAttributes;
+    }
+
+    private static function convertLinks(array $links)
+    {
+        return array_map(function (OCLink $link) {
+            return new Link($link->traceId(), $link->spanId(), [
+                'type' => self::LINK_TYPE_MAP[$link->type()],
+                'attributes' => $link->attributes()
+            ]);
+        }, $links);
+    }
+
+    private static function convertTimeEvents(array $events)
+    {
+        $newEvents = [];
+        foreach ($events as $event) {
+            if ($event instanceof OCAnnotation) {
+                $newEvents[] = self::convertAnnotation($event);
+            } elseif ($event instanceof OCMessageEvent) {
+                $newEvents[] = self::convertMessageEvent($event);
+            }
+        }
+        return $newEvents;
+    }
+
+    private static function convertAnnotation(OCAnnotation $annotation)
+    {
+        return new Annotation($annotation->description(), [
+            'attributes' => $annotation->attributes()
+        ]);
+    }
+
+    private static function convertMessageEvent(OCMessageEvent $messageEvent)
+    {
+        return new MessageEvent($messageEvent->id(), [
+            'type' => self::MESSAGE_TYPE_MAP[$messageEvent->type()],
+            'uncompressedSizeBytes' => $messageEvent->uncompressedSize(),
+            'compressedSizeBytes' => $messageEvent->compressedSize()
+        ]);
+    }
+
+    private static function convertStatus(OCStatus $status = null)
+    {
+        if ($status) {
+            return new Status($status->code(), $status->message());
+        } else {
+            return null;
+        }
     }
 }
--- tests/unit/Stackdriver/SpanConverterTest.php
+++ tests/unit/Stackdriver/SpanConverterTest.php
@@ -18,8 +18,13 @@
 namespace OpenCensus\Tests\Unit\Trace\Exporter\Stackdriver;
 
 use OpenCensus\Trace\Exporter\Stackdriver\SpanConverter;
+use OpenCensus\Trace\Annotation as OCAnnotation;
+use OpenCensus\Trace\Link as OCLink;
+use OpenCensus\Trace\MessageEvent as OCMessageEvent;
 use OpenCensus\Trace\Span as OCSpan;
+use OpenCensus\Trace\Status as OCStatus;
 use Prophecy\Argument;
+use Google\Cloud\Trace\Link;
 use Google\Cloud\Trace\Span;
 use PHPUnit\Framework\TestCase;
 
@@ -59,8 +64,23 @@ class SpanConverterTest extends TestCase
         $this->assertInstanceOf(Span::class, $span);
         $this->assertInternalType('string', $span->name());
         $this->assertInternalType('string', $span->spanId());
+        $this->assertNull($span->parentSpanId());
         $this->assertRegExp('/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{9}Z/', $span->jsonSerialize()['startTime']);
         $this->assertRegExp('/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{9}Z/', $span->jsonSerialize()['endTime']);
+    }
+
+    public function testSetSpanIds()
+    {
+        $span = new OCSpan([
+            'name' => 'span',
+            'spanId' => 'aaa',
+            'parentSpanId' => 'bbb',
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+        $this->assertEquals('0000000000000aaa', $span->spanId());
+        $this->assertEquals('0000000000000bbb', $span->parentSpanId());
     }
 
     public function testFormatsStackTrace()
@@ -104,7 +124,92 @@ class SpanConverterTest extends TestCase
             ['http.port', '80', '/http/port', '80'],
             ['http.path', '/foobar', '/http/url', '/foobar'],
             ['http.method', 'PUT', '/http/method', 'PUT'],
-            ['http.user_agent', 'user agent', '/http/user_agent', 'user agent']
+            ['http.user_agent', 'user agent', '/http/user_agent', 'user agent'],
+            ['random.key', 'some value', 'random.key', 'some value']
         ];
+    }
+
+    public function testLinks()
+    {
+        $span = new OCSpan([
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10,
+            'links' => [
+                new OCLink('traceid', 'spanid', [
+                    'type' => OCLink::TYPE_CHILD_LINKED_SPAN,
+                    'attributes' => [
+                        'foo' => 'bar'
+                    ]
+                ])
+            ]
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+        $data = json_decode(json_encode($span), true)['links']['link'];
+        $this->assertCount(1, $data);
+        $link = $data[0];
+        $this->assertEquals('traceid', $link['traceId']);
+        $this->assertEquals('spanid', $link['spanId']);
+        $this->assertEquals('CHILD_LINKED_SPAN', $link['type']);
+        $this->assertEquals('bar', $link['attributes']['attributeMap']['foo']['stringValue']['value']);
+    }
+
+    public function testTimeEvents()
+    {
+        $span = new OCSpan([
+            'traceId' => 'aaa',
+            'timeEvents' => [
+                new OCAnnotation('some-description', [
+                    'attributes' => ['foo' => 'bar']
+                ]),
+                new OCMessageEvent(OCMessageEvent::TYPE_SENT, 'message-id', [
+                    'uncompressedSize' => 234,
+                    'compressedSize' => 123
+                ])
+            ],
+            'startTime' => new \DateTime(),
+            'endTime' => new \DateTime(),
+            'stackTrace' => []
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+
+        $timeEvents = json_decode(json_encode($span), true)['timeEvents']['timeEvent'];
+        $this->assertCount(2, $timeEvents);
+        $event1 = $timeEvents[0];
+        $this->assertEquals('some-description', $event1['annotation']['description']['value']);
+        $this->assertRegExp('/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{9}Z/', $event1['time']);
+
+        $event2 = $timeEvents[1];
+        $this->assertEquals('SENT', $event2['messageEvent']['type']);
+        $this->assertEquals('message-id', $event2['messageEvent']['id']);
+        $this->assertEquals(234, $event2['messageEvent']['uncompressedSizeBytes']);
+        $this->assertEquals(123, $event2['messageEvent']['compressedSizeBytes']);
+        $this->assertRegExp('/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{9}Z/', $event2['time']);
+    }
+
+    public function testStatus()
+    {
+        $span = new OCSpan([
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10,
+            'status' => new OCStatus(
+                200,
+                'OK'
+            )
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+        $data = json_decode(json_encode($span), true);
+        $this->assertEquals(200, $data['status']['code']);
+        $this->assertEquals('OK', $data['status']['message']);
+    }
+
+    public function testEmptyStatus()
+    {
+        $span = new OCSpan([
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+        $data = json_decode(json_encode($span), true);
+        $this->assertTrue(!isset($data['status']));
     }
 }
