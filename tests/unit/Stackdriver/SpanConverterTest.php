4c1844e96998de6e746947ac436fc1b5c9b8cc98
Jeff Ching <ching.jeff@gmail.com>
2018-04-05 11:31:24 -0700

    Move StackdriverExporter implementation from OpenCensus core (#2)
    * Move StackdriverExporter. Split conversion into SpanConverter
    
    * Fix copyright years
    
    * Convert Links, TimeEvents, Status
    
    * Make timestamp regexp a constant

---
+++ tests/unit/Stackdriver/SpanConverterTest.php
@@ -0,0 +1,217 @@
+<?php
+/**
+ * Copyright 2018 OpenCensus Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenCensus\Tests\Unit\Trace\Exporter\Stackdriver;
+
+use OpenCensus\Trace\Exporter\Stackdriver\SpanConverter;
+use OpenCensus\Trace\Annotation as OCAnnotation;
+use OpenCensus\Trace\Link as OCLink;
+use OpenCensus\Trace\MessageEvent as OCMessageEvent;
+use OpenCensus\Trace\Span as OCSpan;
+use OpenCensus\Trace\Status as OCStatus;
+use Prophecy\Argument;
+use Google\Cloud\Trace\Link;
+use Google\Cloud\Trace\Span;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group trace
+ */
+class SpanConverterTest extends TestCase
+{
+    const TIMESTAMP_FORMAT_REGEXP = '/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{9}Z/';
+
+    /**
+     * @var SpanData[]
+     */
+    private $spans;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->spans = array_map(function ($span) {
+            return $span->spanData();
+        }, [
+            new OCSpan([
+                'name' => 'span',
+                'startTime' => microtime(true),
+                'endTime' => microtime(true) + 10
+            ])
+        ]);
+    }
+
+    public function testFormatsTrace()
+    {
+        $span = new OCSpan([
+            'name' => 'span',
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+
+        $this->assertInstanceOf(Span::class, $span);
+        $this->assertInternalType('string', $span->name());
+        $this->assertInternalType('string', $span->spanId());
+        $this->assertNull($span->parentSpanId());
+        $this->assertRegExp(self::TIMESTAMP_FORMAT_REGEXP, $span->jsonSerialize()['startTime']);
+        $this->assertRegExp(self::TIMESTAMP_FORMAT_REGEXP, $span->jsonSerialize()['endTime']);
+    }
+
+    public function testSetSpanIds()
+    {
+        $span = new OCSpan([
+            'name' => 'span',
+            'spanId' => 'aaa',
+            'parentSpanId' => 'bbb',
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+        $this->assertEquals('0000000000000aaa', $span->spanId());
+        $this->assertEquals('0000000000000bbb', $span->parentSpanId());
+    }
+
+    public function testFormatsStackTrace()
+    {
+        $span = new OCSpan([
+            'stackTrace' => debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS),
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10
+        ]);
+
+        $spans = SpanConverter::convertSpan($span->spanData());
+
+        $data = $spans->jsonSerialize();
+        $this->assertArrayHasKey('stackTrace', $data);
+    }
+
+    /**
+     * @dataProvider attributesToTest
+     */
+    public function testMapsAttributes($key, $value, $expectedAttributeKey, $expectedAttributeValue)
+    {
+        $span = new OCSpan([
+            'attributes' => [
+                $key => $value
+            ],
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10
+        ]);
+
+        $span = SpanConverter::convertSpan($span->spanData());
+
+        $attributes = $span->jsonSerialize()['attributes'];
+        $this->assertArrayHasKey($expectedAttributeKey, $attributes);
+        $this->assertEquals($expectedAttributeValue, $attributes[$expectedAttributeKey]);
+    }
+
+    public function attributesToTest()
+    {
+        return [
+            ['http.host', 'foo.example.com', '/http/host', 'foo.example.com'],
+            ['http.port', '80', '/http/port', '80'],
+            ['http.path', '/foobar', '/http/url', '/foobar'],
+            ['http.method', 'PUT', '/http/method', 'PUT'],
+            ['http.user_agent', 'user agent', '/http/user_agent', 'user agent'],
+            ['random.key', 'some value', 'random.key', 'some value']
+        ];
+    }
+
+    public function testLinks()
+    {
+        $span = new OCSpan([
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10,
+            'links' => [
+                new OCLink('traceid', 'spanid', [
+                    'type' => OCLink::TYPE_CHILD_LINKED_SPAN,
+                    'attributes' => [
+                        'foo' => 'bar'
+                    ]
+                ])
+            ]
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+        $data = json_decode(json_encode($span), true)['links']['link'];
+        $this->assertCount(1, $data);
+        $link = $data[0];
+        $this->assertEquals('traceid', $link['traceId']);
+        $this->assertEquals('spanid', $link['spanId']);
+        $this->assertEquals('CHILD_LINKED_SPAN', $link['type']);
+        $this->assertEquals('bar', $link['attributes']['attributeMap']['foo']['stringValue']['value']);
+    }
+
+    public function testTimeEvents()
+    {
+        $span = new OCSpan([
+            'traceId' => 'aaa',
+            'timeEvents' => [
+                new OCAnnotation('some-description', [
+                    'attributes' => ['foo' => 'bar']
+                ]),
+                new OCMessageEvent(OCMessageEvent::TYPE_SENT, 'message-id', [
+                    'uncompressedSize' => 234,
+                    'compressedSize' => 123
+                ])
+            ],
+            'startTime' => new \DateTime(),
+            'endTime' => new \DateTime(),
+            'stackTrace' => []
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+
+        $timeEvents = json_decode(json_encode($span), true)['timeEvents']['timeEvent'];
+        $this->assertCount(2, $timeEvents);
+        $event1 = $timeEvents[0];
+        $this->assertEquals('some-description', $event1['annotation']['description']['value']);
+        $this->assertRegExp(self::TIMESTAMP_FORMAT_REGEXP, $event1['time']);
+
+        $event2 = $timeEvents[1];
+        $this->assertEquals('SENT', $event2['messageEvent']['type']);
+        $this->assertEquals('message-id', $event2['messageEvent']['id']);
+        $this->assertEquals(234, $event2['messageEvent']['uncompressedSizeBytes']);
+        $this->assertEquals(123, $event2['messageEvent']['compressedSizeBytes']);
+        $this->assertRegExp(self::TIMESTAMP_FORMAT_REGEXP, $event2['time']);
+    }
+
+    public function testStatus()
+    {
+        $span = new OCSpan([
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10,
+            'status' => new OCStatus(
+                200,
+                'OK'
+            )
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+        $data = json_decode(json_encode($span), true);
+        $this->assertEquals(200, $data['status']['code']);
+        $this->assertEquals('OK', $data['status']['message']);
+    }
+
+    public function testEmptyStatus()
+    {
+        $span = new OCSpan([
+            'startTime' => microtime(true),
+            'endTime' => microtime(true) + 10
+        ]);
+        $span = SpanConverter::convertSpan($span->spanData());
+        $data = json_decode(json_encode($span), true);
+        $this->assertTrue(!isset($data['status']));
+    }
+}
--- tests/unit/StackdriverExporterTest.php
+++ tests/unit/StackdriverExporterTest.php
@@ -17,13 +17,70 @@
 
 namespace OpenCensus\Tests\Unit\Trace\Exporter;
 
+require_once __DIR__ . '/mock_error_log.php';
+
 use OpenCensus\Trace\Exporter\StackdriverExporter;
+use OpenCensus\Trace\SpanContext;
+use OpenCensus\Trace\Tracer\TracerInterface;
+use OpenCensus\Trace\Tracer\ContextTracer;
+use OpenCensus\Trace\Span as OCSpan;
+use Prophecy\Argument;
+use Google\Cloud\Trace\Trace;
+use Google\Cloud\Trace\Span;
+use Google\Cloud\Trace\TraceClient;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @group trace
+ */
 class StackdriverExporterTest extends TestCase
 {
-    public function testBasic()
+    /**
+     * @var TraceClient
+     */
+    private $client;
+
+    /**
+     * @var SpanData[]
+     */
+    private $spans;
+
+    public function setUp()
     {
-        $this->markTestSkipped();
+        parent::setUp();
+        $this->client = $this->prophesize(TraceClient::class);
+
+        $this->spans = array_map(function ($span) {
+            return $span->spanData();
+        }, [
+            new OCSpan([
+                'name' => 'span',
+                'startTime' => microtime(true),
+                'endTime' => microtime(true) + 10
+            ])
+        ]);
+    }
+
+    public function testReportWithAnExceptionErrorLog()
+    {
+        $this->client->insert(Argument::any())->willThrow(
+            new \Exception('error_log test')
+        );
+        $trace = $this->prophesize(Trace::class);
+        $trace->setSpans(Argument::any())->shouldBeCalled();
+        $this->client->trace(Argument::any())->willReturn($trace->reveal());
+        $exporter = new StackdriverExporter(
+            ['client' => $this->client->reveal()]
+        );
+        $this->expectOutputString(
+            'Reporting the Trace data failed: error_log test'
+        );
+        $this->assertFalse($exporter->export($this->spans));
+    }
+
+    public function testEmptyTrace()
+    {
+        $exporter = new StackdriverExporter(['client' => $this->client->reveal()]);
+        $this->assertFalse($exporter->export([]));
     }
 }
